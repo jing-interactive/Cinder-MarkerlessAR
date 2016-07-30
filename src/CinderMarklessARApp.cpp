a3fe3457cc22acd12816a54a592528652ee64372
vinjn <vinjn.z@gmail.com>
2016-07-30 22:22:30 +0800

    "Initial commit"

---
+++ src/CinderMarklessARApp.cpp
@@ -0,0 +1,34 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class CinderMarklessARApp : public App {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+};
+
+void CinderMarklessARApp::setup()
+{
+}
+
+void CinderMarklessARApp::mouseDown( MouseEvent event )
+{
+}
+
+void CinderMarklessARApp::update()
+{
+}
+
+void CinderMarklessARApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP( CinderMarklessARApp, RendererGl )
